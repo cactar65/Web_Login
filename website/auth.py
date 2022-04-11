d2f8514ad5a838149f8da896dfca68a84521bbc9
Nick <Cactar65@gmail.com>
2022-04-11 10:13:15 -0400

    added login and cant see home with out login

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("views", __name__)
 
@@ -16,6 +17,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
             else:
                 flash("Inccorect password", category="error")
         else:
@@ -24,8 +27,10 @@ def login():
     return render_template("login.html", boolean=False)
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return render_template("home.html")
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
@@ -50,6 +55,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Account created!", category="success")
             return redirect(url_for("views.home"))
 
